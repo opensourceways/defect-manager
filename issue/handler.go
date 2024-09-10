d41390de25da329fcf72a6237c6f792182879c5b
Coopermassaki <1277145053@qq.com>
2024-09-10 09:06:50 +0800

    test

--- issue/handler.go
+++ issue/handler.go
@@ -250,16 +250,12 @@ func (impl eventHandler) handleIssueOpen(e *sdk.IssueEvent) error {
 
 // || e.Comment.User.Login == impl.botName
 func (impl eventHandler) HandleNoteEvent(e *sdk.NoteEvent) error {
-	fmt.Printf("NOTE EVENT: %v\n", e)
-	fmt.Printf("NOTE EVENT: %v\n", e.Issue)
-	fmt.Printf("NOTE EVENT: %v\n", e.Issue.Body)
-	fmt.Printf("NOTE EVENT: %v\n", impl.cfg.DevelopVersion)
-	//logrus.Infof("handle note event %v", e)
-	/* 	for _, v := range impl.cfg.DevelopVersion {
+	logrus.Infof("handle note event %v", e.Comment.Body)
+	for _, v := range impl.cfg.DevelopVersion {
 		if strings.Contains(e.Issue.Body, v) {
 			return nil
 		}
-	} */
+	}
 
 	if !e.IsIssue() || e.Issue.TypeName != impl.cfg.IssueType ||
 		e.Issue.StateName == StatusFinished || e.Issue.StateName == StatusCancel ||
